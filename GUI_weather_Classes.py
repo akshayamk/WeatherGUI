098fba73b7db82f036e4c13e5770511c6d8e97f6
akshayamk <77065442+akshayamk@users.noreply.github.com>
2021-01-15 13:23:00 +0800

    Add files via upload
    Added more buttons for future forecast

--- GUI_weather_Classes.py
+++ GUI_weather_Classes.py
@@ -31,10 +31,14 @@ class MakeGUI:
         label = tk.Label(lower_frame, font=('Arial', 18), anchor='nw', justify='left', bd=4)
         label.place(relwidth=1, relheight=1)
 
+        scroll = tk.Scrollbar(lower_frame, activebackground='#80c1ff', elementborderwidth=1)
+        scroll.place(relx=0.9, relwidth=0.1, relheight = 1)
+        #mylist = Listbox(lower_frame, yscrollcommand = scro.set )  
+
         button_weather = tk.Button(frame, text='Current Weather', font=40, command=lambda: GetCurrentWeather(entry.get(), label))
         button_weather.place(rely=0.5, relwidth=0.3, relheight=0.5)
 
-        button_forecast = tk.Button(frame, text='Today Hourly Forecast', font=40, command=lambda: GetCurrentWeather(entry.get(), label))
+        button_forecast = tk.Button(frame, text='Next 24 Hours Forecast', font=40, command=lambda: GetHourlyForecast(entry.get(), label))
         button_forecast.place(relx= 0.3, rely=0.5, relwidth=0.4, relheight=0.5)
 
         button_future = tk.Button(frame, text='Next 5 days Forecast', font=40, command=lambda: GetCurrentWeather(entry.get(), label))
@@ -42,7 +46,41 @@ class MakeGUI:
 
        
 
+class GetHourlyForecast:
+    def formate_forecast_response(self, forecast):
+        final_str = ''
+        try:
+            name = forecast['city']['name']
+            final_str += 'City: %s\n' % (name)
+            for i in range(8):
+                datetime = forecast['list'][i]['dt_txt']
+                temperature = forecast['list'][i]['main']['temp']
+                description = forecast['list'][i]['weather'][0]['description']
+
+                final_str += 'Date/Time: %s\nConditions: %s\nTemperature (°C): %s\n\n' % (datetime, description, temperature)
         
+        except:
+            final_str = 'There was a problem retrieving information.\nPlease double check information keyed in.'
+        
+        return final_str
+        
+
+
+    def get_forecast(self, city, label):
+        weather_key = '7076bdf1c054072d1fe38b5c092e2b6e'
+        url = 'http://api.openweathermap.org/data/2.5/forecast'
+        params={'APPID': weather_key, 'q':city, 'units':'metric'}
+        response = requests.get(url, params=params)
+        forecast = response.json()
+
+        label['text']= self.formate_forecast_response(forecast)
+        #self.formate_forecast_response(forecast)
+    
+    def __init__(self, userentry,label):
+        self.label = label
+        self.get_forecast(userentry,label)
+
+
 
 class GetCurrentWeather:
     def format_response(self,weather,airpollution):
@@ -57,7 +95,7 @@ class GetCurrentWeather:
             final_str = 'City: %s\nConditions: %s\nTemperature (°C): %s\nAir Quality Index: %s' % (name, description, temp, aqindex)
 
         except:
-            final_str = 'There was a problem retrieving information.\n Please double check information keyed in.'
+            final_str = 'There was a problem retrieving information.\nPlease double check information keyed in.'
 
         return final_str
 
@@ -85,6 +123,7 @@ class GetCurrentWeather:
 
 def main():
     root = tk.Tk()
+    root.title("Weather App")
     b = MakeGUI(root)
     root.mainloop()
 
